576b5a692637404d5c350bbd4d37e88aaf5a7bad
Aziret Alisher <aziretalisher@gmail.com>
2025-03-02 04:33:50 +0600

    feat: Get products from DynamoDB

--- product-service-backend/handlers/products.js
+++ product-service-backend/handlers/products.js
@@ -1,9 +1,17 @@
-const products = require("./mockData.js");
+const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
+
+const client = new DynamoDBClient({ region: "eu-west-1" });
 
 exports.getProducts = async (event) => {
+  const scanResults = await client.send(new ScanCommand({
+    TableName: "RS_products"
+  }));
+  const items = scanResults.Items.map(item => unmarshall(item));
+
   return {
     statusCode: 200,
     headers: { "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify(products)
+    body: JSON.stringify(items)
   };
 };
