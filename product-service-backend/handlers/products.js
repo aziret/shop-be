85d9e4dd14113ffdbec72be1eab482ff5f17e115
Aziret Alisher <aziretalisher@gmail.com>
2025-03-02 04:59:32 +0600

    feat: Join 2 tables before returning products list

--- product-service-backend/handlers/products.js
+++ product-service-backend/handlers/products.js
@@ -1,4 +1,4 @@
-const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, ScanCommand, BatchGetItemCommand } = require("@aws-sdk/client-dynamodb");
 const { unmarshall } = require("@aws-sdk/util-dynamodb");
 
 const client = new DynamoDBClient({ region: "eu-west-1" });
@@ -7,11 +7,30 @@ exports.getProducts = async (event) => {
   const scanResults = await client.send(new ScanCommand({
     TableName: "RS_products"
   }));
-  const items = scanResults.Items.map(item => unmarshall(item));
+  const products = scanResults.Items.map(item => unmarshall(item));
+
+  const productIds = products.map(product => ({ product_id: { S: product.id } }));
+
+  const stockResults = await client.send(new BatchGetItemCommand({
+    RequestItems: {
+      "RS_stocks": { Keys: productIds }
+    }
+  }));
+
+  const stockItems = stockResults.Responses?.RS_stocks || [];
+  const stockMap = Object.fromEntries(stockItems.map(item => {
+    const stockData = unmarshall(item);
+    return [stockData.product_id, stockData.count];
+  }));
+
+  const finalProducts = products.map(product => ({
+    ...product,
+    stock: stockMap[product.id] || 0 // Default to 0 if no stock data found
+  }));
 
   return {
     statusCode: 200,
     headers: { "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify(items)
+    body: JSON.stringify(finalProducts)
   };
 };
